bc2935d7bcfd29ed0e0e6cf89c509da2e943155c
michael hohl <mhohl@rocketmail.com>
2018-05-29 17:14:33 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(params["owner"])
     end
     @pet.save
     redirect "pets/#{@pet.id}"
@@ -36,6 +36,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
+    @owners = Owner.all
     erb :'/pets/edit'
   end
 end
